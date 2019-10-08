a0e41b289e1204c81199d82633b3c065babdbe04
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-08 13:04:18 -0400

    understanding the children property

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,10 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I'm a {props.name} and I am {props.age} years old!</p>
+    return (
+        <div><p>I'm a {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p></div>
+    )
 };
 
 export default person;

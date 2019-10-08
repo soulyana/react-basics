933482934cd3c497ff422147b1c044670d290ed0
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-08 15:11:33 -0400

    adding styles with stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,11 @@
 // function name is lowercase and same name as componenet
 // stateless "dumb", "presentational" component b/c it has no internal state mgmt
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>

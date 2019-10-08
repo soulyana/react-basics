bc57ce37fd573e895723f6823d05fcc8dcf1064f
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-08 12:12:51 -0400

    JSX restrictions

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      // Only 1 root element that you return
       <div className="App">
         <h1>Hi, I'm a react app</h1>
+        <p>This is really working</p>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Im a react app!!!'));

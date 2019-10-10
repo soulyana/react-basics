ca88e0b08755d3f40841a77b47b80aa224f731fd
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-10 10:02:02 -0400

    assignment2

--- src/App.js
+++ src/App.js
@@ -1,88 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'; // omit js becaue added by build workflow
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
-//stateful "smart", "container" components b/c they contain state of app. Only have a few > easier to maintain 
-
+//stateful "smart", "container" components b/c they contain state of app. Only have a few => easier to maintain 
 class App extends Component {
   state = {
-    persons: [
-      { id: 'abc', name: 'Max', age: 28 },
-      { id: 'def', name: 'Manu', age: 29 },
-      { id: 'ghi', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
-  };
-
-  // two-way binding
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    // new obj copy with old obj info
-    const person = {...this.state.persons[personIndex]};
-    person.name = event.target.value;
-    
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({
-     persons: persons
-    });
+    userInput: ''
   }
 
-  //immutable state update
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice(); // new copy
-    const persons = [...this.state.persons]; // new copied array
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value})
   }
 
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
   }
 
   render() {
-    //inline styles restrict ability to use full css features
-    // use to scope to a style and only applies to a single el
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name} 
-            age={person.age} 
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-          })}
-        </div>
-      );
-    }
+    // splits into an array of strings so map can work 
+    const charList = this.state.userInput.split('').map((char, index) => {
+      return <Char character={char} key={index} clicked={() => this.deleteCharHandler(index)}/>;
+    });
 
     return (
       // Only 1 root element that you return
       <div className="App">
-        <h1>Hi, I'm a react app</h1>
-        <p>This is really working</p>
-        {/* pass a reference dont use () b/c it'll execute immediately */}
-        <button style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        {persons}
+        <input type="text" onChange={this.inputChangedHandler} value={this.state.userInput}/>
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
